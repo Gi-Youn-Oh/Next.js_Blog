ba9efe5b0668c203800d169153915ee55e2850d6
Giyoun Oh <okrldbs@naver.com>
2023-04-15 22:23:41 +0900

    Markdown Highlighter

--- src/components/MarkdownViewer.tsx
+++ src/components/MarkdownViewer.tsx
@@ -1,8 +1,34 @@
+'use client'
+
 import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { materialDark } from 'react-syntax-highlighter/dist/esm/styles/prism'
+import Image from 'next/image'
 
 export default function MarkdownViewer({ content }: { content: string }) {
-    return <ReactMarkdown className="prose lg:prose-xl" remarkPlugins={[remarkGfm]}>
+    return <ReactMarkdown className="prose max-w-none lg:prose-xl" remarkPlugins={[remarkGfm]}
+        components={{
+            code({ node, inline, className, children, ...props }) {
+                const match = /language-(\w+)/.exec(className || '')
+                return !inline && match ? (
+                    <SyntaxHighlighter
+                        {...props}
+                        style={materialDark}
+                        language={match[1]}
+                        PreTag="div"
+                    >
+                        {String(children).replace(/\n$/, '')}
+                    </SyntaxHighlighter>
+                ) : (
+                    <code {...props} className={className}>
+                        {children}
+                    </code>
+                )
+            },
+            img: (image) => <Image className="w-full max-h-60 object-cover"src={image.src || ""} alt={image.alt || ""} width={500} height={350} />
+        }}
+    >
         {content}
     </ReactMarkdown>
 }
