9205b9b352da077161fdb4f02ff79209553e4ce6
Giyoun Oh <okrldbs@naver.com>
2023-04-15 21:43:32 +0900

    Markdown

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import { getPostData } from "@/app/api/posts";
+import MarkdownViewer from "@/components/MarkdownViewer";
 
 type Props = {
     params: {
@@ -6,13 +7,13 @@ type Props = {
     }
 }
 
-export default async function PostPage({params: {slug}}: Props) {
+export default async function PostPage({ params: { slug } }: Props) {
     // 1. 전달된 slug에 해당하는 포스트 데이터 읽어오기
     // 2. 데이터를 마크다운 뷰어에 표기
 
     const posts = await getPostData(slug);
     return <>
-    <h1>{posts.title}</h1>
-    <pre>{posts.content}</pre>
+        <h1>{posts.title}</h1>
+        <MarkdownViewer content = {posts.content} />
     </>
 }
