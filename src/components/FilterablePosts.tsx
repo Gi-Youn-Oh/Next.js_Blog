61bb63c3cf807797479e5b9d9b5124f10cff22d5
Giyoun Oh <okrldbs@naver.com>
2023-04-15 19:35:31 +0900

    Category CSS

--- src/components/FilterablePosts.tsx
+++ src/components/FilterablePosts.tsx
@@ -15,7 +15,7 @@ const ALL_POSTS = 'All Posts';
 export default function FilterablePosts({posts, categories}: Props){
     const [selected, setSelected] = useState(ALL_POSTS);
     const filtered = selected === ALL_POSTS ? posts : posts.filter(post => post.category === selected);
-    return <section>
+    return <section className="flex m-4">
         <PostsGrid posts={filtered} /> 
         <Categories categories={[ALL_POSTS, ...categories]} selected={selected} onClick={(selected) => setSelected(selected)} />
     </section>
