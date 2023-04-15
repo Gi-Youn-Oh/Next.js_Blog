8e6dba252ba5f8c16c5dfa052e793c0e3808705c
Giyoun Oh <okrldbs@naver.com>
2023-04-15 20:22:42 +0900

    Dynamic routing

--- src/app/api/posts.ts
+++ src/app/api/posts.ts
@@ -21,9 +21,23 @@ export async function getNotRecentPosts(): Promise<Post[]> {
 }
 
 export async function getAllPosts(): Promise<Post[]> {
-    const filePath = path.join(process.cwd(), 'data', 'posts.json');
+    const filePath = path.join(process.cwd(), 'data', 'posts.json'); // metadata
     return readFile(filePath, 'utf-8')
         // .then(data => JSON.parse(data))
         .then<Post[]>(JSON.parse) //  생략, json parse type 설정 즉 posts의 타입
         .then(posts => posts.sort((a, b) => (a.date > b.date ? -1 : 1))) // 날짜 정렬
+}
+
+export type PostData = Post & { content: string };
+
+// 실제 md 파일 읽어오기
+export async function getPostData(fileName: string): Promise<PostData> {
+    const filePath = path.join(process.cwd(), 'data','posts', `${fileName}.md`);
+    const metadata = await getAllPosts()
+        .then(posts => posts.find(post => post.path === fileName));
+        if (!metadata) {
+            throw new Error (`${fileName}에 해당하는 포스트를 찾을 수 없음`);
+        }
+        const content = await readFile(filePath, 'utf-8');
+        return {...metadata, content};
 }
