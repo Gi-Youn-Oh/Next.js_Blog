6b53d9cc07aa1c569d21618c4bbf18306558f502
Giyoun Oh <okrldbs@naver.com>
2023-04-16 15:47:12 +0900

    prev&next post logic

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -12,10 +12,14 @@ export default async function PostPage({ params: { slug } }: Props) {
     // 1. 전달된 slug에 해당하는 포스트 데이터 읽어오기
     // 2. 데이터를 마크다운 뷰어에 표기
     const post = await getPostData(slug);
-    const { title, path } = post;
+    const { title, path, next, prev } = post;
 
     return <article className="rounded-2xl overflow-hidden bg-gray-100 shadow-lg m-4">
         <Image className="w-full h-1/5 max-h-[500px]" src={`/images/posts/${path}.png`} alt={title} width={760} height={420} />
         <PostContent post={post} />
+        <section>
+            {prev && <p>{prev.title}</p>}
+            {next && <p>{next.title}</p>}
+        </section>
     </article>
 }
