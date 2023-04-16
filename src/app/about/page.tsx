2fa60762427a3da84945ab1c90c1f0523541c4ad
Giyoun Oh <okrldbs@naver.com>
2023-04-16 18:41:02 +0900

    About page Content add

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,30 +7,47 @@ const CONTENT_CLASS = "text-xl font-bold text-gray-700 m-1";
 export default function About() {
     return (
         <>
-            <Hero/>
+            <Hero />
             <section className="bg-gray-100 shadow-lg p-8 m-8 text-center">
                 <h2 className={TITLE_CLASS}>Who Am I?</h2>
-                <p>개발을 즐기는 프론트엔드 개발자 <br/>
-                함께 나누는 서비스를 추구하며, 사용자 경험에 가치를 두고 있습니다.
+                <p>개발을 즐기는 프론트엔드 개발자 <br />
+                    함께 나누는 서비스를 추구하며, 사용자 경험에 가치를 두고 있습니다.
                 </p>
+
                 <h2 className={TITLE_CLASS}>Click To Link</h2>
                 <div className="flex justify-center my-2">
-                <Link href="https://www.notion.so/6e24c23a25bc4fa7838849ab5d777d1e">
-                <h3 className={CONTENT_CLASS}>Resume |</h3>
-                </Link>
-                <Link href="https://www.notion.so/b87f403ff5ec4e47a7ebfc7dcfbae2d2">
-                <h3 className={CONTENT_CLASS}>About Me |</h3>
-                </Link>
-                <Link href="https://www.notion.so/bd0bd61f95074e20b24800ed7ea7f1bb">
-                <h3 className={CONTENT_CLASS}>Portfolio</h3>
-                </Link>
+                    <Link href="https://www.notion.so/6e24c23a25bc4fa7838849ab5d777d1e">
+                        <h3 className={CONTENT_CLASS}>Resume |</h3>
+                    </Link>
+                    <Link href="https://www.notion.so/b87f403ff5ec4e47a7ebfc7dcfbae2d2">
+                        <h3 className={CONTENT_CLASS}>About Me |</h3>
+                    </Link>
+                    <Link href="https://www.notion.so/bd0bd61f95074e20b24800ed7ea7f1bb">
+                        <h3 className={CONTENT_CLASS}>Portfolio |</h3>
+                    </Link>
+                    <Link href="https://github.com/Gi-Youn-Oh">
+                        <h3 className={CONTENT_CLASS}>GitHub</h3>
+                    </Link>
                 </div>
 
                 <h2 className={TITLE_CLASS}>skills</h2>
-                <p>HTML, CSS, JavaScript, TypeScript
-                <br/>
-                React, ReactNative, Redux, ReactQuery, Next
-                <br/> StyledComponents, TailWind</p>
+                <div className="flex justify-center my-2">
+                    <img className="mx-0.5" src="https://img.shields.io/badge/HTML5-E34F26?style=flat&logo=HTML5&logoColor=white" />
+                    <img className="mx-0.5" src="https://img.shields.io/badge/CSS3-1572B6?style=flat&logo=CSS3&logoColor=white" />
+                    <img className="mx-0.5" src="https://img.shields.io/badge/JavaScript-F7DF1E?style=flat&logo=JavaScript&logoColor=white" />
+                </div>
+                <div className="flex justify-center my-2">
+                    <img className="mx-0.5" src="https://img.shields.io/badge/React-61DAFB?style=flat&logo=React&logoColor=white" />
+                    <img className="mx-0.5" src="https://img.shields.io/badge/ReactQuery-FF4154?style=flat&logo=ReactQuery&logoColor=white" />
+                    <img className="mx-0.5" src="https://img.shields.io/badge/Redux-764ABC?style=flat&logo=Redux&logoColor=white" />
+                    <img className="mx-0.5" src="https://img.shields.io/badge/styledcomponents-DB7093?style=flat&logo=styledcomponents&logoColor=white" />
+                </div>
+                <h3 className={CONTENT_CLASS}>Recent Study</h3>
+                <div className="flex justify-center my-2">
+                    <img className="mx-0.5" src="https://img.shields.io/badge/TypeScript-3178C6?style=flat&logo=TypeScript&logoColor=white" />
+                    <img className="mx-0.5" src="https://img.shields.io/badge/Next-000000?style=flat&logo=Next.js&logoColor=white" />
+                    <img className="mx-0.5" src="https://img.shields.io/badge/Tailwind-06B6D4?style=flat&logo=TailwindCSS&logoColor=white" />
+                </div>
             </section>
         </>
     )
