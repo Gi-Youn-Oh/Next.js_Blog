e66971077771700760b80323cb0bc9d4dffe1dc4
Giyoun Oh <okrldbs@naver.com>
2023-04-17 09:04:56 +0900

    deploy test

--- src/components/RecentPosts.tsx
+++ src/components/RecentPosts.tsx
@@ -2,9 +2,7 @@ import { getRecentPosts } from "@/app/service/posts";
 import PostsGrid from "./PostsGrid";
 
 export default async function RecentPosts() {
-    // 포스트 데이터를 읽어오기
     const posts = await getRecentPosts();
-    // 포스트 데이터를 렌더
 
     return <section className="my-4">
         <h2 className="text-2xl font-bold my-2">Recent Posts</h2>
