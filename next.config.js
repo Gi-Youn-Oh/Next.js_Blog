732cffc274c44236b4126ad33670e7eaa3d2efa3
Giyoun Oh <okrldbs@naver.com>
2023-06-15 16:45:24 +0900

    New Post Auth

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     appDir: true,
   },
   images: {
-    domains: ["img.shields.io","user-images.githubusercontent.com"],
+    domains: ["img.shields.io","user-images.githubusercontent.com","github.com"],
   },
 }
 
