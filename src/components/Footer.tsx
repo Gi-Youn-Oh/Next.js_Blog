15cbe7f7884a2d96bb6b481bf717db06fff887de
Giyoun Oh <okrldbs@naver.com>
2024-07-02 20:27:00 +0900

    ADD: scroll to top button

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,25 @@
+'use client'
+
+import { RxPinTop } from "react-icons/rx";
+
 export default function Footer() {
-    return <p className="bg-gray-700 text-white py-2 text-sm text-center">{"You can look back but don't regret it | All Right Reserved by Giyoun"}</p>
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        });
+    };
+
+    return (
+        <div>
+            <a
+                href=""
+                onClick={() => scrollToTop()}
+                className="fixed bottom-5 right-7 shadow-2xl border border-gray-400 bg-white text-gray-600 p-2 rounded-full hover:bg-gray-100"
+            >
+                <RxPinTop size={30} />
+            </a>
+            <p className="bg-gray-700 text-white py-2 text-sm text-center">{"You can look back but don't regret it | All Right Reserved by Giyoun"}</p>
+        </div>
+    );
 }
