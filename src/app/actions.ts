6b167584faca96fa8b28d575267c487222f0b27c
Giyoun Oh <okrldbs@naver.com>
2024-07-05 01:27:20 +0900

    ADD: pending disabled, comments sorting, optimistic UI

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,10 +1,15 @@
+"use server";
+
 import { checkSession } from "@/utils/session";
 import { supabase } from "@/utils/superbase";
 import { revalidatePath } from "next/cache";
-import { NextResponse } from "next/server";
 
-export async function deleteComment(created_at: string, post_path: string) {
-  "use server";
+type actionResponse = {
+  status: number;
+  message?: string;
+};
+
+export async function deleteComment(created_at: string, post_path: string): Promise<actionResponse> {
 
   const sessionResponse = await checkSession();
   if (sessionResponse) {
@@ -17,10 +22,8 @@ export async function deleteComment(created_at: string, post_path: string) {
     .match({ created_at });
 
   if (error) {
-    return NextResponse.json(
-      { message: "댓글 삭제에 실패했습니다." },
-      { status: 500 }
-    );
+    return { status: 500, message: "댓글 삭제에 실패했습니다." }
+      ;
   }
 
   revalidatePath(`/api/posts/${post_path}`);
@@ -31,8 +34,7 @@ export async function updateComment(
   created_at: string,
   updatedComment: string,
   post_path: string
-) {
-  'use server'
+): Promise<actionResponse> {
 
   const sessionResponse = await checkSession();
   if (sessionResponse) {
@@ -41,17 +43,14 @@ export async function updateComment(
 
   const { error } = await supabase
     .from("comment")
-    .update({comment: updatedComment} )
+    .update({ comment: updatedComment })
     .eq("created_at", created_at)
     .select();
 
   if (error) {
-    return NextResponse.json(
-      { message: "댓글 수정에 실패했습니다." },
-      { status: 500 }
-    );
+    return { status: 500, message: "댓글 수정에 실패했습니다." }
   }
-  
+
   revalidatePath(`/api/posts/${post_path}`);
   return { status: 200, message: "댓글이 수정되었습니다." };
 }
