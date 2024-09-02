35255a00d798cd70b0ef0f97d65ecd5cf1c93855
Giyoun Oh <okrldbs@naver.com>
2024-09-02 22:06:28 +0900

    FIX: hydrating issue

--- src/components/comments/CommentCard.tsx
+++ src/components/comments/CommentCard.tsx
@@ -3,8 +3,7 @@
 import { useState, startTransition, useOptimistic } from "react";
 import { HiOutlinePencilSquare } from "react-icons/hi2";
 import { FiTrash2 } from "react-icons/fi";
-import {Comment, formatDate, PurifyComment} from "@/app/service/comment";
-import { useSession } from "next-auth/react";
+import { PurifyComment} from "@/app/service/comment";
 import ReactLinkify from 'react-linkify';
 
 interface CommentCardProps {
@@ -86,7 +85,6 @@ export default function CommentCard({
                   {comment.name}
                 </span>
                 <span className="mt-0.5 text-xs text-gray-500">
-                  {formatDate(comment.created_at)}
                 </span>
               </div>
               {comment.isEditable && (
