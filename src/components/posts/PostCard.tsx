0666e78c80f1110ad90498236d475c13d3ac1034
Giyoun <dhrldbs2679@gmail.com>
2024-09-24 13:19:51 +0900

    MODIFY: PostCard h3 css

--- src/components/posts/PostCard.tsx
+++ src/components/posts/PostCard.tsx
@@ -9,7 +9,7 @@ export default function PostCard({ post: { title, description, date, category, p
         <Image className="w-full" src={`/images/posts/${path}.png`} alt={title} width={300} height={200} quality={100}/>
         <div className="flex flex-col items-center p-4">
             <time className="self-end test-gray-700">{date.toString()}</time>
-            <h3 className="text-lg font-bold">{title}</h3>
+            <h3 className="w-full text-center text-lg truncate font-bold">{title}</h3>
             <p className="w-full truncate text-center">{description}</p>
             <span className="text-sm rounded-lg bg-green-100 px-2 my-2">{category}</span>
         </div>
