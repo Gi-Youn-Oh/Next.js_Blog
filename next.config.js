d2532e3b15a5daffd3eb654ebd4d2e4f5b868607
기윤 <dhrldbs2679@gmail.com>
2024-10-11 21:24:11 +0900

    REMOVE: next-pwa

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-const withPWA = require('next-pwa')({
-  dest: 'public',
-})
-
 const nextConfig = {
   images: {
     remotePatterns: [
@@ -23,4 +19,4 @@ const nextConfig = {
   }
 }
 
-module.exports = withPWA(nextConfig);
+module.exports = nextConfig;
