047e54ee13b93428466a4d38b4a0009080bdc530
기윤 <dhrldbs2679@gmail.com>
2024-10-11 21:10:29 +0900

    TEST: service worker regist

--- next.config.js
+++ next.config.js
@@ -1,13 +1,28 @@
 /** @type {import('next').NextConfig} */
 
-const runtimeCaching = require('next-pwa/cache');
-const withPWA = require('next-pwa')({
+const withPWAInit = require("next-pwa");
+const isDev = process.env.NODE_ENV !== "production";
+
+const withPWA = withPWAInit({
   dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-  register: true,
-  skipWaiting: true,
-  runtimeCaching,
-})
+  disable: isDev,
+
+  exclude: [
+    // add buildExcludes here
+    ({ asset, compilation }) => {
+      if (
+          asset.name.startsWith("server/") ||
+          asset.name.match(/^((app-|^)build-manifest\.json|react-loadable-manifest\.json)$/)
+      ) {
+        return true;
+      }
+      if (isDev && !asset.name.startsWith("static/runtime/")) {
+        return true;
+      }
+      return false;
+    }
+  ],
+});
 
 const nextConfig = {
   images: {
