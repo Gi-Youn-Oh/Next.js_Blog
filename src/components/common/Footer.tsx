e7dbfb30b5a186f82f565e2151800cf14eb351b7
기윤 <dhrldbs2679@gmail.com>
2024-11-09 21:20:31 +0900

    MODIFY: about page

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,9 +1,28 @@
 'use client'
 
 import { RxPinTop } from "react-icons/rx";
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 export default function Footer() {
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+        const toggleVisibility = () => {
+            if (window.scrollY > 300) {
+                setIsVisible(true);
+            } else {
+                setIsVisible(false);
+            }
+        };
+
+        window.addEventListener('scroll', toggleVisibility);
+
+        // 컴포넌트 언마운트 시 스크롤 이벤트 제거
+        return () => {
+            window.removeEventListener('scroll', toggleVisibility);
+        };
+    }, []);
+
     const scrollToTop = () => {
         window.scrollTo({
             top: 0,
@@ -13,6 +32,7 @@ export default function Footer() {
 
     return (
         <div>
+            {isVisible && (
             <button
                 type="button"
                 aria-label={"Scroll to top"}
@@ -20,7 +40,7 @@ export default function Footer() {
                 className="fixed bottom-5 right-7 shadow-2xl border border-gray-400 bg-white text-gray-400 p-2 rounded-full hover:bg-gray-100"
             >
                 <RxPinTop size={30} />
-            </button>
+            </button>)}
             <p className="bg-gray-700 text-white py-2 text-sm text-center">{"You can look back but don't regret it | All Right Reserved by Giyoun"}</p>
         </div>
     );
