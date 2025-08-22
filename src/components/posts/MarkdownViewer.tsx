9ab40ae70cc75db269a96f453c587700ef9fe414
incento_giyoun <dhrldbs2679@gmail.com>
2025-08-22 17:00:40 +0900

    TEST: upgrade markdown

--- src/components/posts/MarkdownViewer.tsx
+++ src/components/posts/MarkdownViewer.tsx
@@ -1,48 +1,57 @@
-'use client'
+"use client";
 
-import ReactMarkdown from 'react-markdown'
-import remarkGfm from 'remark-gfm'
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
-import { coldarkDark } from 'react-syntax-highlighter/dist/esm/styles/prism'
-import Image from 'next/image'
-import rehypeRaw from 'rehype-raw'
-
-// import { Light as SyntaxHighlighter } from 'react-syntax-highlighter'
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { coldarkDark } from "react-syntax-highlighter/dist/esm/styles/prism";
+import Image from "next/image";
+import rehypeRaw from "rehype-raw";
 
 export default function MarkdownViewer({ content }: { content: string }) {
-    return <ReactMarkdown className="prose max-w-none break-words lg:prose-lg" remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw]}
+  return (
+    <div className="prose max-w-none break-words lg:prose-lg">
+      <ReactMarkdown
+        remarkPlugins={[remarkGfm]}
+        rehypePlugins={[rehypeRaw]}
         components={{
-            code({ node, inline, className, children, ...props }) {
-                const match = /language-(\w+)/.exec(className || '')
-                return !inline && match ? (
-                    <SyntaxHighlighter
-                        {...props}
-                        style={coldarkDark}
-                        language={match[1]}
-                        PreTag="div"
-                        // wrapLines={true}
-                        wrapLongLines={true}
-                    >
-                        {String(children).replace(/\n$/, '')}
-                    </SyntaxHighlighter>
-                ) : (
-                    <code {...props} className={className}>
-                        {children}
-                    </code>
-                )
-            },
-            img: (image: any) =>
-                <Image
-                    className={image.alt === "exception" ? "w-full h-auto max-w-[1500px]" : "w-full h-auto max-w-[900px]"}
-                    src={image.src || ""}
-                    alt={image.alt || ""}
-                    width={1000}
-                    height={700}
-                    quality={100}
-                    unoptimized={image.alt === "exception" ? true : false}
-                />
+          code({ node, inline, className, children, ...props }:any) {
+            const match = /language-(\w+)/.exec(className || "");
+            return !inline && match ? (
+              <SyntaxHighlighter
+                {...props}
+                style={coldarkDark}
+                language={match[1]}
+                PreTag="div"
+                // wrapLines={true}
+                wrapLongLines={true}
+              >
+                {String(children).replace(/\n$/, "")}
+              </SyntaxHighlighter>
+            ) : (
+              <code {...props} className={className}>
+                {children}
+              </code>
+            );
+          },
+          img: (image: any) => (
+            <Image
+              className={
+                image.alt === "exception"
+                  ? "w-full h-auto max-w-[1500px]"
+                  : "w-full h-auto max-w-[900px]"
+              }
+              src={image.src || ""}
+              alt={image.alt || ""}
+              width={1000}
+              height={700}
+              quality={100}
+              unoptimized={image.alt === "exception" ? true : false}
+            />
+          ),
         }}
-    >
+      >
         {content}
-    </ReactMarkdown>
+      </ReactMarkdown>
+    </div>
+  );
 }
