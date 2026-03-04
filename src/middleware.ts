a0d069357e4e5e8f78dc669f890d80fd23a995ce
기윤 <dhrldbs2679@gmail.com>
2026-03-04 12:54:38 +0900

    ADD: test bot

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,17 @@ import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(req: NextRequest) {
+  const ua = req.headers.get('user-agent') || ''
+
+  if (
+    ua.includes('Google') ||
+    ua.includes('AdsBot') ||
+    ua.includes('facebookexternalhit') ||
+    ua.includes('Facebot')
+  ) {
+    console.log('BOT DETECTED:', ua)
+  }
+
   const token = await getToken({ req });
   // if already logged in, try to access the signin page, redirect to home
   if (token) {
